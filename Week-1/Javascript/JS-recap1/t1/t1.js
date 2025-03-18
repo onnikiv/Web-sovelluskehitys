a92b91efe2c0f398973102137d29c9a4828e06ca
onnik <onni.kivinen@gmail.com>
2025-03-18 17:33:10 +0200

    t1 done

--- Week-1/Javascript/JS-recap1/t1/t1.js
+++ Week-1/Javascript/JS-recap1/t1/t1.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const celsius = Number(prompt('Enter temperature in Celsius:'));
+const fahrenheit = celsius * (9 / 5) + 32;
+const kelvin = celsius + 273.15;
+const p = document.createElement('a');
+
+p.textContent = celsius + 'C equals to ' + fahrenheit + 'F, ' + kelvin + 'K.';
+document.body.appendChild(p);
