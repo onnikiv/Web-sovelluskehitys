eda09227d0ad130ff6fded6f2d63876524fd6493
onnik <onni.kivinen@gmail.com>
2025-03-18 17:34:31 +0200

    t1 done

--- Week-1/Javascript/JS-recap1/t1/t1.js
+++ Week-1/Javascript/JS-recap1/t1/t1.js
@@ -5,5 +5,6 @@ const fahrenheit = celsius * (9 / 5) + 32;
 const kelvin = celsius + 273.15;
 const p = document.createElement('a');
 
-p.textContent = celsius + 'C equals to ' + fahrenheit + 'F, ' + kelvin + 'K.';
+p.textContent =
+  celsius + ' C equals to ' + fahrenheit + ' F, ' + kelvin + ' K.';
 document.body.appendChild(p);
