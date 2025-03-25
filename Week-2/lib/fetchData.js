93bf1f4a4399f0b7e57e8c63478d8eff44ee5404
onnik <onni.kivinen@gmail.com>
2025-03-25 09:49:51 +0200

    Add JSDoc comments to fetchData function

--- Week-2/lib/fetchData.js
+++ Week-2/lib/fetchData.js
@@ -1,3 +1,11 @@
+/**
+ * Fetches data from the given URL with the specified options.
+ *
+ * @param {string} URL - The URL to fetch data from.
+ * @param {Object} [options] - The options to pass to the fetch request.
+ * @returns {Promise<Object>} The JSON response from the fetch request.
+ * @throws {Error} If the response is not ok.
+ */
 export async function fetchData(URL, options) {
   const response = await fetch(URL, options);
   if (!response.ok) {
