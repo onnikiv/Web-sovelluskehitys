3659f68bb73e6025acdda3aa92f2ca0169abf9bb
onnik <onni.kivinen@gmail.com>
2025-03-26 10:19:20 +0200

    t1 done // GET

--- Week-2/JS-recap5/t1/t1.js
+++ Week-2/JS-recap5/t1/t1.js
@@ -3,9 +3,15 @@ import {fetchData} from '../../lib/fetchData.js';
 async function init() {
   try {
     const url = 'https://reqres.in/api/users/1';
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
-    const data = await fetchData(url);
-    console.log(data);
+    const response = await fetchData(url, options);
+    console.log(response);
   } catch (error) {
     console.error('An error occurred:', error);
   }
