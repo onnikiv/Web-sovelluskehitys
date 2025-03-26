849e3cd0fe35bf30b72471f71d02584ebc9eccf1
onnik <onni.kivinen@gmail.com>
2025-03-26 13:09:42 +0200

    Enhance fetchData function to improve error handling and return JSON response

--- Week-2/lib/fetchData.js
+++ Week-2/lib/fetchData.js
@@ -6,10 +6,14 @@
  * @returns {Promise<Object>} The JSON response from the fetch request.
  * @throws {Error} If the response is not ok.
  */
-export async function fetchData(url, options) {
+export async function fetchData(url, options = {}) {
   const response = await fetch(url, options);
+  const json = await response.json();
   if (!response.ok) {
-    throw new Error(`${response.statusText}, code: ${response.json}`);
+    if (json.message) {
+      throw new Error(`${json.message}, code:${response.status}`);
+    }
+    throw new Error(`Error ${response.status} occured!`);
   }
-  return await response.json();
+  return json;
 }
